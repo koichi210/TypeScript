98f5f7be58ac169cd048d2eb36431fd8e55d5a2c
koichi210 <koichi210@gmail.com>
2020-09-17 10:16:40 +0900

    TypeScriptでClass作成

---
+++ Class/app.js
@@ -0,0 +1,11 @@
+// classを指定してクラス化
+class Main {
+    // コンストラクター
+    constructor() {
+        console.log("Hello!!World.");
+        console.log("execute Node.js  from TypeScript code");
+    }
+}
+// Mainクラスのインスタンスを作る
+const main = new Main();
+//# sourceMappingURL=app.js.map
